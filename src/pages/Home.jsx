84d9fca34e470676d6a1a7bece8003776e152a2a
21040631-NguyenPhiVu <zozotrong123456@gmail.com>
2025-12-30 00:09:23 +0700

    Add Feature Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from "react";
 import BannerCarousel from "../components/Banner/BannerCarousel";
 import { getBanners } from "../services/bannerService";
+import FeatureSection from "../components/FeatureSection";
+import { getFeatures } from "../services/featureService";
+
 const Home = () => {
     const [banners, setBanners] = useState([]);
+    const [features, setFeatures] = useState([]);
 
     useEffect(() => {
         getBanners().then(setBanners);
+        getFeatures().then(setFeatures);
     }, []);
 
-    return <BannerCarousel banners={banners} />;
+    return (
+        <>
+            {/* Banner Section */}
+            <BannerCarousel banners={banners} />
+
+            {/* Feature Section */}
+            <FeatureSection features={features} />
+        </>
+    );
 }
 
 export default Home;
