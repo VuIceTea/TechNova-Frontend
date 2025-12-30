cf2659b87b599a08e1b4059feaf286d32d30aa48
21040631-NguyenPhiVu <zozotrong123456@gmail.com>
2025-12-30 14:19:46 +0700

    Add Category Section

---
+++ src/data/mockCategory.js
@@ -0,0 +1,27 @@
+export const mockCategories = [
+    {
+        id: 1,
+        name: "Máy tính",
+        icon: "computer",
+    },
+    {
+        id: 2,
+        name: "Điện thoại",
+        icon: "smartphone",
+    },
+    {
+        id: 3,
+        name: "Âm thanh",
+        icon: "headphones",
+    },
+    {
+        id: 4,
+        name: "Đồng hồ",
+        icon: "watch",
+    },
+    {
+        id: 5,
+        name: "Nhà thông minh",
+        icon: "home_iot",
+    }
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,18 @@ import BannerCarousel from "../components/Banner/BannerCarousel";
 import { getBanners } from "../services/bannerService";
 import FeatureSection from "../components/FeatureSection";
 import { getFeatures } from "../services/featureService";
+import CategorySection from "../components/CategorySection";
+import { getCategories } from "../services/categoryService";
 
 const Home = () => {
     const [banners, setBanners] = useState([]);
     const [features, setFeatures] = useState([]);
+    const [categories, setCategories] = useState([]);
 
     useEffect(() => {
         getBanners().then(setBanners);
         getFeatures().then(setFeatures);
+        getCategories().then(setCategories);
     }, []);
 
     return (
@@ -20,6 +24,9 @@ const Home = () => {
 
             {/* Feature Section */}
             <FeatureSection features={features} />
+
+            {/* Category Section */}
+            <CategorySection categories={categories} />
         </>
     );
 }
